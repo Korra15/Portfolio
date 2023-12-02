6c8e4e1f7535e93ca3c023229e2aef59f2bfe836
Korra15 <mailayushi15@gmail.com>
2023-12-02 21:59:24 +0530

    Responsive triumphs

--- components/triumphs.tsx
+++ components/triumphs.tsx
@@ -24,7 +24,7 @@ export default function Triumphs() {
   return (
     <motion.section
       ref={ref}
-      className="mb-28 w-[60rem] text-justify leading-8 sm:mb-40 scroll-mt-28" 
+      className="mb-28 max-w-[60rem] text-justify leading-8 sm:mb-40 scroll-mt-28" 
       initial={{ opacity: 0, y: 100 }}
       animate={{ opacity: 1, y: 0 }}
       transition={{ delay: 0.175 }}
