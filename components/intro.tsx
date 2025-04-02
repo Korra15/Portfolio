0cc51d7a9eadb9effbc70b9392f9ee1e49288849
Ayushi Parikh <mailayushi15@gmail.com>
2025-04-02 13:50:18 -0400

    2- Adding itch link

--- components/intro.tsx
+++ components/intro.tsx
@@ -100,18 +100,18 @@ export default function Intro() {
 
           <a
             className="bg-white p-4 text-gray-700 flex items-center gap-2 text-[1.35rem] rounded-full focus:scale-[1.15] hover:scale-[1.15] hover:text-gray-950 active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10 dark:text-white/60"
-            href="https://the-way-to-wisdom.blogspot.com/"
-            target="_blank"
-          >
-            <FaBlogger  />
-          </a>
-
-          <a
-            className="bg-white p-4 text-gray-700 flex items-center gap-2 text-[1.35rem] rounded-full focus:scale-[1.15] hover:scale-[1.15] hover:text-gray-950 active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10 dark:text-white/60"
             href="https://korra15.itch.io/"
             target="_blank"
           >
             <FaGamepad  />
+          </a>
+          
+          <a
+            className="bg-white p-4 text-gray-700 flex items-center gap-2 text-[1.35rem] rounded-full focus:scale-[1.15] hover:scale-[1.15] hover:text-gray-950 active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10 dark:text-white/60"
+            href="https://the-way-to-wisdom.blogspot.com/"
+            target="_blank"
+          >
+            <FaBlogger  />
           </a>
 
         </div>
